f2d8de8096fe7152c38f738dfa3b63144b15c343
saadmalik4058 <131814900+saadmalik4058@users.noreply.github.com>
2024-10-30 06:07:13 -0700

    Update Todo.jsx

--- Redux-Toolkit-main/src/Components/Todo.jsx
+++ Redux-Toolkit-main/src/Components/Todo.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../Features/Todos/TodoSlice";
+import { removeTodo, updateTodo } from "../Features/Todos/TodoSlice";
 
 function Todo() {
   const todos = useSelector((state) => state.todos);
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
+  function handleEdit(id) {
+    dispatch(updateTodo({ id, text: editText }));
+    setEditId(null);
+    setEditText("");
+  }
+
   const dispatch = useDispatch();
   return (
     <div className="max-w-lg mx-auto p-4">
@@ -16,13 +24,45 @@ function Todo() {
             key={todo.id}
             className="flex justify-between items-center p-3 bg-white rounded shadow-md hover:bg-gray-100 transition"
           >
-            <span className="text-gray-700 text-lg">{todo.text}</span>
-            <button
-              onClick={() => dispatch(removeTodo(todo.id))}
-              className="px-3 py-1 bg-red-500 text-white rounded-full hover:bg-red-600 transition"
+            {editId == todo.id ? (
+              <input
+                type="text"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+                className="text-gray-700 text-lg border rounded px-2"
+              />
+            ) : (
+              <span className="text-gray-700 text-lg">{todo.text}</span>
+            )}
+            <div
+              className="flex
+             gap-3 "
             >
-              X
-            </button>
+              {editId == todo.id ? (
+                <button
+                  onClick={() => handleEdit(todo.id)}
+                  className="px-3 py-1 bg-red-500 text-white rounded-full hover:bg-red-600 transition"
+                >
+                  Save
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    setEditId(todo.id);
+                    setEditText(todo.text);
+                  }}
+                >
+                  Edit
+                </button>
+              )}
+
+              <button
+                onClick={() => dispatch(removeTodo(todo.id))}
+                className="px-3 py-1 bg-red-500 text-white rounded-full hover:bg-red-600 transition"
+              >
+                X
+              </button>
+            </div>
           </li>
         ))}
       </ul>
